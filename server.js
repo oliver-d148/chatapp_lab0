05be825d83d6f80549892bbf1269ea12a3a53ba5
Oliver <oliver.blunck@smail.th-koeln.de>
2025-04-02 21:17:23 +0200

    ChatApp Topic Darstellung

--- server.js
+++ server.js
@@ -35,21 +35,32 @@ app.get("/topic", (req, res) => {
 app.get('/topic/:id', (req, res) => {
     const chatId = req.params.id;
 
-    const sql = 'SELECT sender, content FROM inhalt WHERE chat_id = ?';
+    //const sql = 'SELECT sender, content FROM inhalt WHERE chat_id = ?';
+    const sql = `
+        SELECT c.Topic, i.sender, i.content 
+        FROM chats c 
+        LEFT JOIN inhalt i ON c.ID = i.chat_id 
+        WHERE c.ID = ?;
+    `;
+
     db.query(sql, [chatId], (err, results) => {
         if (err) return res.status(500).json({ message: "Fehler beim Abrufen", error: err });
-        res.json(results);
+        //res.json(results);
+        const topic_Name = results[0].Topic;
+
+        res.json({ topic: topic_Name, chats: results });
     });
 });
 
 // POST-Route für Formulardaten
 app.post("/submit", (req, res) => {
     const { topic } = req.body;
+    const topic2 = req.body.topic;
 
-    if (!topic) return res.status(400).json({ message: "Topic ist erforderlich" });
+    if (!topic2) return res.status(400).json({ message: "Topic ist erforderlich" });
 
     const sql = "INSERT INTO chats (Topic) VALUES (?)";
-    db.query(sql, [topic], (err, result) => {
+    db.query(sql, [topic2], (err, result) => {
         if (err) return res.status(500).json({ message: "Fehler beim Speichern", error: err });
         //res.json({ message: "Daten erfolgreich gespeichert!" });
     });
@@ -61,16 +72,13 @@ app.post("/submit-message", (req, res) => {
     const scontent2 = req.body.content;
     const chat_id2 = req.body.chat_id;
 
-    if (!sender || !content ) {
-        return res.status(400).json({ message: "Alle Felder sind erforderlich!" });
-    }
+    if (!sender || !content ) return res.status(400).json({ message: "Alle Felder sind erforderlich!" });
+    
 
     const sql = "INSERT INTO inhalt (sender, content, chat_id) VALUES (?, ?, ?)";
     db.query(sql, [sender2, scontent2, chat_id2], (err, result) => {
-        if (err) {
-            return res.status(500).json({ message: "Fehler beim Speichern", error: err });
-        }
-        res.json({ message: "Nachricht erfolgreich gespeichert!" });
+        if (err) return res.status(500).json({ message: "Fehler beim Speichern", error: err });
+        //res.json({ message: "Nachricht erfolgreich gespeichert!" });
     });
 });
 
